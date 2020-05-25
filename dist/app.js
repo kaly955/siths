34f10d69e5acd13ad42481b31a4d29b777d60c3a
kaly955 <57500215+kaly955@users.noreply.github.com>
2020-05-25 03:45:22 -0400

    s

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+console.log('Connected');
+
+var myVar;
+    
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
